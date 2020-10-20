e751bf5e349f4f6d6b81659f55f5bfe338d487a6
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-10-20 17:23:39 +0200

    refactor - return the string value of the int

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,7 +6,6 @@ class FizzBuzz
     public function count($int):string
     {
         if ($int === 3)return "Fizz";
-        if($int === 2)return "2";
-        return "1";
+        return strval($int);
     }
 }

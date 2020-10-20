492704893a94c1727134857537271958b602c103
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-10-20 17:36:37 +0200

    add - count for input 6

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -6,7 +6,7 @@ class FizzBuzz
     public function count($int):string
     {
         if ($int === 5)return "Buzz";
-        if ($int === 3)return "Fizz";
+        if ($int === 3 || $int === 6)return "Fizz";
         return strval($int);
     }
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -47,9 +47,9 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
         return $this->fizzBuzz->count($int);
     }
 
-    public function test_count_given6_shouldReturn6(){
+    public function test_count_given6_shouldReturnFizz(){
         $actual=$this->actCount(6);
-        $this->assertEquals("6", $actual);
+        $this->assertEquals("Fizz", $actual);
     }
 
 }

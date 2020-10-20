df7a7704972e0f2b01e9de0abd68de1c47a8a76a
Thibaud <thibaud.d.walggraeve@student.vinci.be>
2020-10-20 17:00:43 +0200

    add - count for input 2

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,6 +5,7 @@ class FizzBuzz
 {
     public function count($int):string
     {
+        if($int === 2)return "2";
         return "1";
     }
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -9,10 +9,20 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
 
     public function test_count_given1_shouldReturn1(){
+        //A
+        $fizzBuzz = new FizzBuzz();
+        //Act
+        $actual = $fizzBuzz->count(1);
+        //Assert
+        $this->assertEquals("1",$actual);
+    }
+
+    public function test_count_given2_shouldReturn2(){
         $fizzBuzz = new FizzBuzz();
 
-        $actual = $fizzBuzz->count(1);
+        $actual = $fizzBuzz->count(2);
 
-        $this->assertEquals("1",$actual);
+        $this->assertEquals("2",$actual);
+
     }
 }
